41200ad3e8a7ccdb4821ecd95a02fd6a09bbd3dd
Михаил Петров <mikhailpetrov80@gmail.com>
2022-04-20 16:26:14 +0400

    First Commit

---
+++ src/main/java/org/example/ClientSocketApp.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientSocketApp {
+
+    public static void main(String[] args) {
+
+        String host = "localhost";
+        int port = 8080;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Netology");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
