ccea0b2e9b9fb86229b8ca416eb1adadc4a0bfc5
Михаил Петров <mikhailpetrov80@gmail.com>
2022-04-21 07:15:04 +0400

    new Commit

--- src/main/java/org/example/SocketApp.java
+++ src/main/java/org/example/SocketApp.java
@@ -13,14 +13,17 @@ public class SocketApp {
         int port = 8080;
 
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("New connection accepted");
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-            serverSocket.close();
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                System.out.println("New connection accepted");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //serverSocket.close();
         }
     }
 }
